1ff616ed9680fdf9a301cbc65d738efe54c691f3
Olga Bolotova <olbolot@gmail.com>
2020-10-15 22:50:12 +0300

    Add animation for cities

--- src/scripts/addPopupForCity.js
+++ src/scripts/addPopupForCity.js
@@ -1,24 +1,31 @@
 export default function addPopupForCity() {
   const wrapper = document.querySelector('.js-cities');
-  const icons = document.querySelectorAll('[data-icon]');
+  let selectorIconsWrapper = window.innerWidth >= 768 ? '.js-image-on-desktop' : '.js-image-on-mobile';
+  let icons = wrapper.querySelectorAll(selectorIconsWrapper + ' [data-icon]');
+
+  function resizeArea(icon) {
+    const area = document.querySelector(`[data-area="${icon.dataset.icon}"]`);
+    const iconHeight = icon.getBoundingClientRect().bottom - icon.getBoundingClientRect().top;
+    const iconWidth = icon.getBoundingClientRect().right - icon.getBoundingClientRect().left;
+
+    area.style.height = iconHeight.toFixed() + 'px';
+    area.style.width = iconWidth.toFixed() + 'px';
+    area.style.top = icon.getBoundingClientRect().top - wrapper.getBoundingClientRect().top + 'px';
+    area.style.left = icon.getBoundingClientRect().left - wrapper.getBoundingClientRect().left + 'px';
+  }
 
   for (const icon of icons) {
-    const area = document.querySelector(`[data-area="${icon.dataset.icon}"]`);
+    resizeArea(icon);
 
-    function resizeArea() {
-      const iconHeight = icon.getBoundingClientRect().bottom - icon.getBoundingClientRect().top;
-      const iconWidth = icon.getBoundingClientRect().right - icon.getBoundingClientRect().left;
-      area.style.height = iconHeight.toFixed() + 'px';
-      area.style.width = iconWidth.toFixed() + 'px';
-      area.style.top = icon.getBoundingClientRect().top - wrapper.getBoundingClientRect().top + 'px';
-      area.style.left = icon.getBoundingClientRect().left - wrapper.getBoundingClientRect().left + 'px';
-    }
-
-    window.addEventListener('scroll', resizeArea);
-    window.addEventListener('resize', resizeArea);
-
-    area.addEventListener('click', () => {
-      console.log(10)
-    });
+    window.addEventListener('scroll', resizeArea.bind(this, icon));
   }
+
+  window.addEventListener("resize", () => {
+    selectorIconsWrapper = window.innerWidth >= 768 ? '.js-image-on-desktop' : '.js-image-on-mobile';
+    icons = wrapper.querySelectorAll(selectorIconsWrapper + ' [data-icon]');
+
+    for (const icon of icons) {
+      resizeArea(icon);
+    }
+  });
 }
