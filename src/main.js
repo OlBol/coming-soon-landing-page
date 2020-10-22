278aaff003629e826d48f4e72bf05e0b639df4d6
olbolot <olbolot@gmail.com>
2020-10-22 15:30:26 +0300

    Add open graph protocol

--- src/main.js
+++ src/main.js
@@ -10,10 +10,29 @@ addPopupForCity();
 slider();
 showLangs();
 
-setTimeout(() => {
-    // document.querySelector('.is-load').style.display = 'block';
-    $('.is-load').fadeIn(200);
-    // document.querySelector('.js-preloader').classList.remove('is-active');
-    $('.js-preloader').fadeOut(200);
-}, 3000);
-// }, 25);
+// setTimeout(() => {
+//     $('.is-load').fadeIn(200);
+//     $('.js-preloader').fadeOut(200);
+// }, 3000);
+
+
+// const images = document.querySelectorAll('img');
+// let imagesCount = 0;
+//
+// for (const image of images) {
+//     image.addEventListener('load', () => {
+//         console.log(image);
+//         imagesCount++;
+//     });
+// }
+// if (imagesCount === images.length) {
+//     $('.is-load').fadeIn(200);
+//     $('.js-preloader').fadeOut(200);
+// }
+
+
+window.onload = function() {
+    // $('.is-load').fadeIn(200);
+    $('.is-load').css({'display': 'block'});
+    $('.js-preloader').fadeOut(400);
+};
